90b9d31173b393c7b3f88a517537b1c206fec8f2
Mossblac <Murphyrmurphy2000@gmail.com>
2025-06-27 09:58:36 -0600

    the function works, needless requirements have been added by test

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,36 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    #if os.path.abspath(directory).startswith(os.path.abspath(working_directory)) == True:
-        #return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-    if os.path.isdir(directory) == False:
-        return (f'Error: "{directory}" is not a directory')
-    contents = os.listdir(directory)
-    if len(contents) == 0:
-        return ('Error: directory is empty')
+    if directory == None:
+        directory = "."
+    try:
+        wk = os.path.abspath(working_directory)
+        d = os.path.abspath(directory)
+    except Exception:
+        return 'Error: os.path.abspath failed'
+    if d.startswith(wk):
+        if os.path.isdir(d) == False:
+            return (f'Error: "{directory}" is not a directory')
+        try:
+            contents = os.listdir(d)
+        except Exception:
+            return ('Error: listdir() failed' )
+        if len(contents) == 0:
+            return ""
+        else:
+            file_list = []
+            for file in contents:
+                file_name = file
+                try:
+                    file_size = os.path.getsize(os.path.join(d,file))
+                except Exception:
+                    return "Error: os.path.getsize() returned error"
+                if os.path.isdir(os.path.join(d,file)):
+                    is_dir = True
+                else:
+                    is_dir = False
+                file_string = f'- {file_name}: file_size={file_size} bytes, is_dir={is_dir}'
+                file_list.append(file_string)
     else:
-        content_string = ""
-        for file in contents:
-            file_name = file
-            file_size = os.path.getsize(file)
-            if os.path.isfile(file) == True:
-                is_dir = False
-            else:
-                is_dir = True
-            file_string = f'- {file_name}: file_size={file_size} bytes, is_dir={is_dir}'
-    return content_string + file_string
+        return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    return "\n".join(file_list) 
