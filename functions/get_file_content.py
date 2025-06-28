9c4dfb7ee5ab78856420d251a76ac84d16dcbb99
Mossblac <Murphyrmurphy2000@gmail.com>
2025-06-27 19:19:24 -0600

    get_files_content tested working, created write_files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -20,7 +20,7 @@ def get_file_content(working_directory, file_path):
     trunc_message = f'...File "{file_path}" truncated at 10000 characters'
     
     if d_norm == wk_norm or d_norm.startswith(wk_norm + os.sep):
-        #try:
+        try:
             if os.path.isfile(f):
                 with open(f, "r") as file:
                     file_content_string = file.read()
@@ -32,8 +32,8 @@ def get_file_content(working_directory, file_path):
                         return file_content_string
             else:
                 return f'Error: File not found or is not a regular file: "{f}"'        
-        #except Exception:
-            #return "Error: os.path.isfile failed or file failed to read"
+        except Exception:
+            return "Error: os.path.isfile failed or file failed to read"
         
     else:
         return (f'Error: Cannot list "{file_path}" as it is outside the permitted working directory')
--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 from functions.get_file_content import get_file_content
 
 def main():
-  print(get_file_content("calculator", "lorem.txt"))
-  
+  print(get_file_content("calculator", "main.py"))
+  print("--------------------------------------")
+  print(get_file_content("calculator", "pkg/calculator.py"))
+  print("--------------------------------------")
+  print(get_file_content("calculator", "/bin/cat"))
+  print("--------------------------------------")
 
 
 
