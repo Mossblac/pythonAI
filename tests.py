fdc0ef2996800b31fbc217d71c35286efc25dc2c
Mossblac <Murphyrmurphy2000@gmail.com>
2025-07-02 17:33:45 -0600

    run_python function tested working

--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
-from functions.write_files import write_file
+from functions.run_python import run_python_file
 
 def main():
-  print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+  print(run_python_file("calculator", "main.py"))
   print("--------------------------------------")
-  print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+  print(run_python_file("calculator", "tests.py"))
   print("--------------------------------------")
-  print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+  print(run_python_file("calculator", "../main.py"))
   print("--------------------------------------")
+  print(run_python_file("calculator", "nonexistent.py"))
 
 
 
