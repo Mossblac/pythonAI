a45187d5570331772fe806ca2ef2d009ca82d160
Mossblac <Murphyrmurphy2000@gmail.com>
2025-07-02 19:29:21 -0600

    config changed with system instruction: JUST A ROBOT

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google import genai
 from google.genai import types
 
 def main():
+    system_prompt = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
     parser = argparse.ArgumentParser(description="a scripts with a verbose option")
     parser.add_argument("prompt")
     parser.add_argument("--verbose", action="store_true", help="increase output verbosity")
@@ -21,7 +22,9 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     x = response.usage_metadata.prompt_token_count
     y = response.usage_metadata.candidates_token_count
